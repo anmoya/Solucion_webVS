123944ee4126fc1d861aebe98123dd1fd8da0c7f
Alfonso <anmoya@uc.cl>
2017-08-22 10:04:29 -0300

    Agregar archivos de proyecto.

---
+++ BibliotecaClases/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaClases
+{
+    public class Persona
+    {
+        public string nombre;
+        public string apellido;
+
+        public Persona()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            this.nombre = string.Empty;
+            this.apellido = string.Empty;
+        }
+
+
+
+        
+    }
+}
